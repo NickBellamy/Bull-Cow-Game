562a44212f5628a71404db8da97f5f9f9240510b
Nick Bellamy <nick.bellamy@gmail.com>
2017-01-03 12:23:48 +0000

    Refactor code to tidy main function

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,20 +3,37 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+//Entry point for application
 int main()
 {
-	//Introduce the game
+	PrintIntro();
+
+	GetGuessAndPrintBack();
+
+	cout << endl;
+	return 0;
+}
+
+//Introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n\n";
+	return;
+}
 
-	//Get a guess from the player
-	string Guess = "";
+//Get a guess from the player
+string GetGuessAndPrintBack()
+{
 	cout << "Enter your guess: ";
+	string Guess = "";
 	getline(cin, Guess);
 
-	//Repeat the guess back to them
+	//Print the guess back to player
 	cout << "Your guess was: " << Guess << endl;
-
-	return 0;
+	return Guess;
 }

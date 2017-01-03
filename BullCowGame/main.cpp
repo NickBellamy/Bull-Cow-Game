00edf93a3abbdb1f8b39fe89cf116f69d54e6055
Nick Bellamy <nick.bellamy@gmail.com>
2017-01-03 12:48:09 +0000

    Allow multiple guesses to be performed by player

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -11,24 +11,31 @@ int main()
 {
 	PrintIntro();
 
-	GetGuessAndPrintBack();
-
-	cout << endl;
+	//Loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		GetGuessAndPrintBack();
+		cout << endl;
+	}
+	
 	return 0;
 }
 
-//Introduce the game
+
 void PrintIntro()
 {
+	//Introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n\n";
 	return;
 }
 
-//Get a guess from the player
+
 string GetGuessAndPrintBack()
 {
+	//Get a guess from the player
 	cout << "Enter your guess: ";
 	string Guess = "";
 	getline(cin, Guess);

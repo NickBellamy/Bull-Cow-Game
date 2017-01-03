2217fe4a2d33278964e59aa150c8956b69312dad
Nick Bellamy <nick.bellamy@gmail.com>
2017-01-03 10:55:04 +0000

    Add ability for player to guess a word

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	//Introduce the game
 	constexpr int WORD_LENGTH = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game\n";
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n\n";
+
+	//Get a guess from the player
+	string Guess = "";
+	cout << "Enter your guess: ";
+	cin >> Guess;
+
+	//Repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
 	return 0;
 }

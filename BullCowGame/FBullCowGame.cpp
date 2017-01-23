9d713b3536721de071b2e848fc2d2e384826965f
Nick Bellamy <nick.bellamy@gmail.com>
2017-01-23 21:07:47 +0000

    Handle cases where guess is not all lowercase

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -29,7 +29,7 @@ EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
 	{
 		return EGuessStatus::Not_Isogram;
 	}
-	else if (false) // if the guess isn't all lowercase 
+	else if (!IsLowercase(Guess)) // if the guess isn't all lowercase 
 	{
 		return EGuessStatus::Not_Lowercase; // TODO write function
 	}
@@ -106,5 +106,17 @@ bool FBullCowGame::IsIsogram(FString Word) const
 	}
 
 	return true; // for example in cases where /0 is entered
+}
+
+bool FBullCowGame::IsLowercase(FString Word) const
+{
+	for (auto Letter : Word)
+	{
+		if (!islower(Letter) || Letter != ' ')
+		{
+			return false;
+		}
+		return true;
+	}
 }
 
